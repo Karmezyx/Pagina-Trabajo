1a5fc20850025728ad0ec781439722a7bee5a282
Karmezyx <132037426+Karmezyx@users.noreply.github.com>
2023-05-03 00:24:06 -0400

    Add files via upload

--- Formulario.php
+++ Formulario.php
@@ -1,8 +1,34 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title>PHP Test</title>
+	<title>Registro exitoso</title>
 </head>
 <body>
-<?php echo '<p>Hello World</p>'; ?>
+	<?php
+
+  class Solicitud
+    public $nombre
+    public $email
+    public $password
+    public $confirm_password
+
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			$nombre = $_POST["nombre"];
+			$email = $_POST["email"];
+			$contrasena = $_POST["contrasena"];
+			$sexo = $_POST["sexo"];
+
+			echo "<p>Gracias por registrarte</p>";
+			echo "<p>Los datos registrados son:</p>";
+			echo "<ul>";
+			echo "<li>Nombre: $nombre</li>";
+			echo "<li>Email: $email</li>";
+			echo "<li>Contraseña: $contrasena</li>";
+			echo "<li>Sexo: $sexo</li>";
+			echo "</ul>";
+		}
+	?>
 </body>
 </html>
+
+
